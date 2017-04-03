02d407744b165c6c93b78957ba4f2eeb16640108
Sam Gilmour <spg4@st-andrews.ac.uk>
2017-04-03 21:24:52 +0100

    Add first lines of an attempt at drawing the board.

--- src/Draw.hs
+++ src/Draw.hs
@@ -9,4 +9,14 @@ import Board
 -- This will need to extract the Board from the world state and draw it
 -- as a grid plus pieces.
 drawWorld :: World -> Picture
-drawWorld w = Color blue $ Circle 100
+-- drawWorld w = Color blue $ line [(0, 0), (10, 10)]
+drawWorld world =
+
+drawLines :: Position -> Position -> Position -> Picture
+drawLines (a, b) (c, d) (x, y) = [line [(a + x * i, b + y * i), (c + x * i, d + y * i)] |  i <- [0..19]]
+
+drawGrid :: Picture
+drawGrid = pictures [
+                      drawLines (-475, -475) (475, -475) (0, 50),
+                      drawLines (-475, -475) (-475, 475) (50, 0)
+                    ]
