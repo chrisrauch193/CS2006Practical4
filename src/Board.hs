3e815ec44c55c905291d3985a99635606e2157e5
Jalaj Khandelwal <jalajkhandelwal@8afbf91a.st-andrews.ac.uk>
2017-04-06 10:28:28 +0100

    Fixed the program not working

--- src/Board.hs
+++ src/Board.hs
@@ -104,11 +104,24 @@ checkWon board
   | otherwise = Just (y)
   where
     (x,y) = head (pieces board)
-    allMoves = map (checkfunction x y (pieces board) 0) directionList
+    allDirectionMoves = map (checkfunction x y (pieces board) 0) directionList
+    allMoves = getTotal allDirectionMoves [] 0
     condition = filter (fiveExist) allMoves
 
+getTotal :: [Int] -> [Int] -> Int-> [Int]
+getTotal individualMoves calculatedMoves directionNumber
+  | directionNumber == 8 = calculatedMoves
+  | otherwise = getTotal individualMoves newCalcMoves newDirNumber
+  where
+    intOneToAdd = individualMoves !! directionNumber
+    intTwoToAdd = individualMoves !! (directionNumber + 1)
+    intToAdd = intOneToAdd + intTwoToAdd
+    newDirNumber = directionNumber + 2
+    newCalcMoves = intToAdd : calculatedMoves
+
+
 fiveExist :: Int -> Bool
-fiveExist listInt = listInt== 5
+fiveExist listInt = listInt >= 6
 
 checkfunction :: Position -> Col-> [(Position,Col)] -> Int-> Position-> Int
 checkfunction (xCheck,yCheck) colToCheck listOfPieces numPieces (aToAdd,bToAdd)
